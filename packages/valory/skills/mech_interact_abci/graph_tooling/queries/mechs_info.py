2cddcd3eb7ce20a41d8c20f8190cd8889c60f01f
Adamantios <adamantioszaras@gmail.com>
2025-10-31 18:27:09 +0200

    feat: add query for fetching the mech info from the subgraph

---
+++ packages/valory/skills/mech_interact_abci/graph_tooling/queries/mechs_info.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2025 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Mechs' info queries."""
+
+
+from string import Template
+
+
+# TODO add karma and max delivery rate when implemented on the subgraph
+info = Template(
+    """
+        {
+            meches(
+                first: ${first},
+                orderBy: id,
+                orderDirection: asc,
+                where: {id_gt: "${mechs_id_gt}", service_: {totalDeliveries_gt: 0}}
+            ) {
+                address
+                service {
+                    id
+                    totalRequests
+                    totalDeliveries
+                    metadata {
+                        metadata
+                    }
+                }
+            }
+        }
+    """
+)
